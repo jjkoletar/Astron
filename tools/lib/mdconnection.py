402750d9fe1838f1ab3003fcfa51ccc6d3e39688
Kestred <kestred@riotcave.com>
2013-09-04 02:49:28 -0600

    query-tool: allow opening of connection to server

--- tools/lib/mdconnection.py
+++ tools/lib/mdconnection.py
@@ -1,10 +1,21 @@
-import socket
+import errno
+from socket import socket, AF_INET, SOCK_STREAM
+from socket import error as socket_error
+
 __all__ = ['MDConnection']
 
 class MDConnection(object):
-    def __init__(self, sock):
-        self.s = sock
-        self.s.settimeout(0.1)
+    def connect(self, location="localhost"):
+        if location == "localhost":
+            location = '127.0.0.1'
+        self._sock = socket(AF_INET, SOCK_STREAM)
+        try:
+            self._sock.connect((location, 7199))
+        except socket_error as serr:
+            if serr.errno == errno.ECONNREFUSED:
+                return "Connection refused"
+        self._sock.settimeout(0.1)
+        return "" # Connected successfuly
 
     def send(self, datagram):
         data = datagram.get_data()
@@ -31,10 +42,13 @@ class MDConnection(object):
         return result
 
     def close(self):
-        self.s.close()
+        self._sock.close()
 
     def flush(self):
         while self._read(): pass
 
     def read(self):
         return Datagram(self._read())
+
+    def set_timeout(self, timeout):
+        self._sock.settimeout(timeout)
--- tools/server_query.py
+++ tools/server_query.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 import os, sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/lib")
 
@@ -6,7 +6,8 @@ from datagram import *
 from mdconnection import *
 
 HELP = [("help", "get these help commands"),
-        ("exit | quit", "Quit the query session.")]
+        ("exit | quit", "Quit the query session."),
+        ("connect", "connect to OpenOTP server at IP address")]
 
 class Queryer(object):
     def __init__(self):
@@ -18,13 +19,45 @@ class Queryer(object):
             command = raw_input(self._prompt_string())
         except EOFError:
             print "\nRecieved EOF"
-            sys.exit(0)
+            self._exit()
+        self.query(command)
 
+    def query(self, command):
         if command == "exit" or command == "quit":
-            sys.exit(0)
+            self._exit()
+
+        # Help lists help commands
         if command == "help":
             for entry in HELP:
                 print "  %s - %s" % (entry[0], entry[1])
+
+        # Connect to localhost by default
+        if command == "connect":
+            self._connect("localhost")
+
+        # Connect to an IP address or 'localhost'
+        if command[:len("connect ")] == "connect ":
+            if len(command) > len("connect "):
+                self._connect(command[len("connect "):])
+
+    def _connect(self, location):
+        if len(self._location) == 0:
+            self._location = location
+            self._connection = MDConnection()
+            message = self._connection.connect(self._location)
+            if len(message) > 0:
+                print "Connection to " + location + " failed:"
+                print " "*8 + message
+                self._location = ""
+        else:
+            print "Not connecting"
+            print "Previous connection exists to: " + self._location
+
+    def _exit(self):
+        if len(self._location) > 0:
+            self._connection.close()
+            print "Closed connection to " + self._location
+        sys.exit(0)
 
     def _prompt_string(self):
         loc = ""
